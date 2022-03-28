9facad88719a31657f5b69228e968f747318817d
82108 <82108@192.168.0.89>
2022-03-28 09:57:00 +0900

    2022-03-25 강의분 커밋

--- myhome/src/main/java/com/godcoder/myhome/controller/BoardController.java
+++ myhome/src/main/java/com/godcoder/myhome/controller/BoardController.java
@@ -1,16 +1,53 @@
 package com.godcoder.myhome.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.godcoder.myhome.model.Board;
+import com.godcoder.myhome.repository.BoardRepository;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 	
+	@Autowired
+	private BoardRepository boardRepository;
+	
 	@GetMapping("/list")
-	public String list() {
+	public String list(Model model) {
+		List<Board> boards = boardRepository.findAll();
+		model.addAttribute("boards", boards);
 		return "board/list";
+	}
+	
+	@GetMapping("/form")
+	public String form(Model model, @RequestParam(required = false) Long id) {
+		if(id == null) {
+			model.addAttribute("board", new Board());
+		}else {
+			Board board = boardRepository.findById(id).orElse(null);
+			model.addAttribute("board", board);
+		}
+		return "board/form";
+	}
+	
+	@PostMapping("/form")
+	public String greetingSubmit(@Valid Board board, BindingResult bindingResult) {
+		if(bindingResult.hasErrors()) {
+			return "board/form";
+		}
+		boardRepository.save(board);
+		return "redirect:/board/list";
 	}
 
 }
